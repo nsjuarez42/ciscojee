48ebc800e740fc0aa40bf1bfa4527b3c953f8302
nsjuarez42 <63726225+nsjuarez42@users.noreply.github.com>
2020-09-08 20:59:14 -0300

    Create Pais.java

---
+++ clase2/Biblioteca/src/main/java/org/cisco/jee/biblioteca/Pais.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cisco.jee.biblioteca;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class Pais {
+    private String nombre;
+            
+     public Pais(String nombre){
+     this.nombre = nombre;}
+     
+     public String getNombre(){
+     return this.nombre;}
+     
+     public void setNombre(String nombre){
+         this.nombre = nombre;
+     }
+}
