5648507d6c3fe8c4f54ebdcdc7154c6b96ccf55d
Pablo Frias <pablosfrias@gmail.com>
2020-09-08 19:54:10 -0300

    Ejemplo Maven - Clase 2 de JEE

---
+++ clase2/Biblioteca/src/main/java/org/cisco/jee/biblioteca/Biblioteca.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cisco.jee.biblioteca;
+
+/**
+ *
+ * @author pablo
+ */
+public class Biblioteca {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+    
+}
